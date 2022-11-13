4d85ff5990bb38c3b47540cfd24f900e380562fe
Ivan Kruchkov <rizident88@gmail.com>
2022-11-13 13:38:59 +0700

    add example 005 IfElse

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")// ToLower перевод в нижний регистр
+{
+    Console.WriteLine("Ура это же Маша");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
